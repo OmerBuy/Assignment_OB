7852cf17ae6013e0c137a5d166980f9720bfa366
kavelasvilli <osbuyruk@gmail.com>
2020-01-31 14:51:10 +0200

    Update qqqqqqqqqqqqqqqqqqqq.java

--- assignments/day_10_assignment5/qqqqqqqqqqqqqqqqqqqq.java
+++ assignments/day_10_assignment5/qqqqqqqqqqqqqqqqqqqq.java
@@ -5,6 +5,7 @@ public class qqqqqqqqqqqqqqqqqqqq {
 	public static void main(String[] args) {
 		System.out.println("aaaaaaaaaaaaaa");
 		System.out.println("aaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaaa");
 
 	}
 
